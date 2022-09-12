921e86e116f4cfbff517b0645ec88933a1c3f561
HirokiYasui <hiromaily2@gmail.com>
2022-09-12 16:54:50 +0900

    add login form

---
+++ login-form/src/components/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { LoginForm } from './LoginForm'
+import { Box } from '@mui/material'
+
+export const App: FC = () => {
+  return (
+    <Box>
+      <LoginForm />
+    </Box>
+  )
+}
---
+++ login-form/src/components/LoginForm.tsx
@@ -0,0 +1,59 @@
+import { FC } from 'react'
+import { useForm } from 'react-hook-form'
+import { Button, TextField } from '@mui/material'
+
+export const LoginForm: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
+
+  return (
+    <>
+      {/* フォームサブミット前に、handleSubmit が input
+      コントロールの検証を行います。 */}
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '250px',
+          margin: '3rem',
+        }}
+      >
+        {/* register 関数を利用し、input コントロールを hook に登録します。 */}
+        <TextField
+          {...register('nameRequired', { required: true })}
+          error={!!errors.nameRequired}
+          id='name'
+          label='Name'
+          variant='outlined'
+          helperText={!!errors.nameRequired && 'Input your name'}
+          style={{
+            marginBottom: '1rem',
+          }}
+        />
+        <TextField
+          {...register('passwordRequired', { required: true })}
+          error={!!errors.passwordRequired}
+          id='password'
+          label='Password'
+          variant='outlined'
+          type='password'
+          helperText={!!errors.passwordRequired && 'Input your password'}
+          style={{
+            marginBottom: '1rem',
+          }}
+        />
+        <Button variant='contained' color='primary' type='submit'>
+          Submit
+        </Button>
+      </form>
+    </>
+  )
+}
